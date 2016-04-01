4f5ec17fb9f3bcbf5f30e51557096703e5edada0
Brian Kaplan <bakaplan2@gmail.com>
2016-03-31 22:33:21 -0400

    first commit

---
+++ app/src/main/java/com/brian/hokiediner/OauthCostants.java
@@ -0,0 +1,11 @@
+package com.brian.hokiediner;
+
+/**
+ * Created by brian on 3/17/16.
+ */
+public class OauthCostants {
+    final static String REDIRECT_URL = "http://wso2.com";
+    final static String CLIENT_ID = "Fx4jb8VckK2qAW6gsAge2QNrWMUa";
+    final static String CLIENT_SECRET = "juLUSerU0ohaS2NiZGQc5CMYkM8a";
+    final static String TRUSTSTORE_PASSWORD = "wso2carbon";
+}
